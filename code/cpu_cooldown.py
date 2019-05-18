e0979cc4079accc55a0fa4ce907bd6caf5d28f2f
Ahmed Bilal Khalid <ahmedbilal96@gmail.com>
2019-05-18 21:09:54 +0500

    Initial COde added

--- code/cpu_cooldown.py
+++ code/cpu_cooldown.py
@@ -1,1 +1,9 @@
-# Your code here
+import os
+
+def read_temp():
+	filepath = "/sys/class/thermal/thermal_zone0/temp"
+	with open(filepath, "r") as f:
+		temp = int(f.read())/1000
+		return temp
+
+print(read_temp(), "C")
